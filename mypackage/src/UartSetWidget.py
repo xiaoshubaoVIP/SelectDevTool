695e30f1d320378f71c3474affb1970cee53d2e0
xiaoshubao <1379472873@qq.com>
2026-04-15 15:26:13 +0800

    1.备份软件;

--- mypackage/src/UartSetWidget.py
+++ mypackage/src/UartSetWidget.py
@@ -4,7 +4,8 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QWidget, QListWidget, QStackedWidget, QHBoxLayout, QFormLayout, QLineEdit, QRadioButton, \
-    QLabel, QCheckBox, QApplication, QDesktopWidget, QButtonGroup, QVBoxLayout, QPushButton, QSizePolicy, QMessageBox
+    QLabel, QCheckBox, QApplication, QDesktopWidget, QButtonGroup, QVBoxLayout, QPushButton, QSizePolicy, QMessageBox, \
+    QComboBox
 from pandas.io.pytables import Fixed
 
 
@@ -18,17 +19,30 @@ class UartSetWidget(QWidget):
         self.table_display_data = None
 
         # 数据
-        self.line_edit_com = QLineEdit()
-        self.line_edit_baud = QLineEdit()
-        self.line_edit_data_bit = QLineEdit()
-        self.line_edit_stop_bit = QLineEdit()
-        self.line_edit_parity_bit = QLineEdit()
+        self.cb_com = QComboBox()
+        self.cb_com.setFixedSize(200, 30)
+        self.cb_com.setStyleSheet("QComboBox { background-color: white; }")
+        self.cb_com.addItems(["COM3"])
 
-        self.line_edit_com.setPlaceholderText("COM3")
-        self.line_edit_baud.setPlaceholderText("9600")
-        self.line_edit_data_bit.setPlaceholderText("Data8")
-        self.line_edit_stop_bit.setPlaceholderText("OneStop")
-        self.line_edit_parity_bit.setPlaceholderText("NoParity")
+        self.cb_baud = QComboBox()
+        self.cb_baud.setFixedSize(200, 30)
+        self.cb_baud.setStyleSheet("QComboBox { background-color: white; }")
+        self.cb_baud.addItems(["9600"])
+
+        self.cb_data_bit = QComboBox()
+        self.cb_data_bit.setFixedSize(200, 30)
+        self.cb_data_bit.setStyleSheet("QComboBox { background-color: white; }")
+        self.cb_data_bit.addItems(["Data8"])
+
+        self.cb_stop_bit = QComboBox()
+        self.cb_stop_bit.setFixedSize(200, 30)
+        self.cb_stop_bit.setStyleSheet("QComboBox { background-color: white; }")
+        self.cb_stop_bit.addItems(["OneStop"])
+
+        self.cb_parity_bit = QComboBox()
+        self.cb_parity_bit.setFixedSize(200, 30)
+        self.cb_parity_bit.setStyleSheet("QComboBox { background-color: white; }")
+        self.cb_parity_bit.addItems(["NoParity"])
 
         self.push_button_1 = QPushButton('连接串口')
         self.push_button_1.setStyleSheet("background-color: rgb(255,255,255); color: black;")
@@ -40,7 +54,6 @@ class UartSetWidget(QWidget):
         self.push_button_2.setFixedSize(100, 40)
         self.push_button_2.clicked.connect(self.bt_save)
 
-
         layout = QVBoxLayout(self)
         layout_1 = QVBoxLayout(self)
         layout_1_1 = QHBoxLayout(self)
@@ -50,16 +63,30 @@ class UartSetWidget(QWidget):
         layout_1_5 = QHBoxLayout(self)
         layout_2 = QHBoxLayout(self)
 
-        layout_1_1.addWidget(QLabel("端口"))
-        layout_1_1.addWidget(self.line_edit_com)
-        layout_1_2.addWidget(QLabel("波特率"))
-        layout_1_2.addWidget(self.line_edit_baud)
-        layout_1_3.addWidget(QLabel("数据位"))
-        layout_1_3.addWidget(self.line_edit_data_bit)
-        layout_1_4.addWidget(QLabel("停止位"))
-        layout_1_4.addWidget(self.line_edit_stop_bit)
-        layout_1_5.addWidget(QLabel("奇偶校验位"))
-        layout_1_5.addWidget(self.line_edit_parity_bit)
+        label_com = QLabel("端口")
+        label_com.setFixedWidth(100)
+        layout_1_1.addWidget(label_com)
+        layout_1_1.addWidget(self.cb_com)
+
+        label_baud = QLabel("波特率")
+        label_baud.setFixedWidth(100)
+        layout_1_2.addWidget(label_baud)
+        layout_1_2.addWidget(self.cb_baud)
+
+        label_data_bit = QLabel("数据位")
+        label_data_bit.setFixedWidth(100)
+        layout_1_3.addWidget(label_data_bit)
+        layout_1_3.addWidget(self.cb_data_bit)
+
+        label_stop_bit = QLabel("停止位")
+        label_stop_bit.setFixedWidth(100)
+        layout_1_4.addWidget(label_stop_bit)
+        layout_1_4.addWidget(self.cb_stop_bit)
+
+        label_parity_bit = QLabel("奇偶校验位")
+        label_parity_bit.setFixedWidth(100)
+        layout_1_5.addWidget(label_parity_bit)
+        layout_1_5.addWidget(self.cb_parity_bit)
 
         layout_1.addLayout(layout_1_1)
         layout_1.addLayout(layout_1_2)
@@ -69,8 +96,8 @@ class UartSetWidget(QWidget):
 
         layout_2.addWidget(self.push_button_1)
         layout_2.addWidget(self.push_button_2)
-        layout_2.addWidget(self.push_button_1, alignment=Qt.AlignHCenter)
-        layout_2.addWidget(self.push_button_2, alignment=Qt.AlignHCenter)
+        # layout_2.addWidget(self.push_button_1, alignment=Qt.AlignHCenter)
+        # layout_2.addWidget(self.push_button_2, alignment=Qt.AlignHCenter)
 
         layout.addLayout(layout_1)
         layout.addLayout(layout_2)
@@ -80,7 +107,7 @@ class UartSetWidget(QWidget):
         self.setWindowTitle('串口设置')
         #self.setFixedSize(1, 1)
         self.show()# 或者show()
-        self.setGeometry(300, 100, 720, 480)
+        self.setGeometry(300, 100, 360, 240)
         self.center()
 
     # @staticmethod
@@ -104,6 +131,7 @@ class UartSetWidget(QWidget):
 def main():
     app = QApplication(sys.argv)
     ex = UartSetWidget()
+    ex.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
